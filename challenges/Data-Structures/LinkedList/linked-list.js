2fdf447a0c12bc75447bbfcad0930a6d56d3ad1b
Ahmad Mamdouh ALhabrawi <mamdouh486@gmail.com>
2020-09-22 16:55:40 +0300

    edit

--- challenges/Data-Structures/LinkedList/linked-list.js
+++ challenges/Data-Structures/LinkedList/linked-list.js
@@ -13,9 +13,17 @@ class LinkedList {
   }
   insert(value) {
     const node = new Node(value);
-    node.next = this.head;
-    this.head = node;
-    return this.head;
+    if (!this.head) {
+      this.head = node;
+      return this;
+    }   
+
+    let current = this.head;
+    while (current.next){
+      current = current.next;
+    }
+    current.next = node;
+    return this;
   }
 
   includes(value){
@@ -43,98 +51,7 @@ class LinkedList {
     }
     return string + ` -> { ${current.next} }`;
   }
-
-  append(value){
-    const node = new Node(value);
-    if (!this.head) {
-      this.head = node;
-      return this.head;
-    }   
-
-    let current = this.head;
-    while (current.next){
-      current = current.next;
-    }
-    current.next = node;
-    return this.head;
-  }
-
-  insertBefore(value, newVal){
-    const addedNode = new Node(newVal);
-
-    let current = this.head;
-
-    if(value === current.value){
-      this.insert(newVal);
-    } else {
-      while (current.next){
-        let prev = current;
-        current = current.next;
-        if (value === current.value){
-          addedNode.next = current;
-          prev.next = addedNode;
-          return prev;
-        }
-      }
-    }
-  }
-
-  insertAfter(value, newVal){
-    const addedNode = new Node(newVal);
-
-    let current = this.head;
-
-    if(value === current.value){
-      this.append(newVal);
-    } else {
-      while (current.next){
-        current = current.next;
-        let prev = current;
-        if (value === current.value){
-          addedNode.next = current.next;
-          prev.next = addedNode;
-        }
-      }
-    }
-  }
-
-  kthFromEnd(k){
-    let counter = 0; 
-    if ((typeof(k) === 'number') && k>=0){
-      if (this.head){
-        counter ++;
-        let current = this.head;
-        let anotherCurrent = this.head;
-        while(current.next){
-          current = current.next;
-          counter++;
-        }
-        for (let i = 0; i<(counter-k-1); i++){
-          anotherCurrent = anotherCurrent.next;
-        }
-        if ((counter-k-1)<0){
-          return 'exception';
-        }
-        return anotherCurrent.value;
-  
-      } else {
-        return 'empty linked list';
-      }
-    } else {
-      return 'invalid value';
-    }
-  } 
-
 }
-
-// const linkedListInstance = new LinkedList();
-// linkedListInstance.append(1);
-// linkedListInstance.append(2);
-// linkedListInstance.append(3);
-// linkedListInstance.append(5);
-// console.log(linkedListInstance.insertAfter(3,4));
-// console.log(linkedListInstance.toString());
-// console.log(linkedListInstance.kthFromEnd(0));
 
 
 module.exports = LinkedList;
