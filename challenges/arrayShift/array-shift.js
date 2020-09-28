be2b19d54b5cfe386ba62bb574e3e3abe08c1db2
Ahmad Mamdouh ALhabrawi <mamdouh486@gmail.com>
2020-09-28 19:26:45 +0300

    done

--- challenges/arrayShift/array-shift.js
+++ challenges/arrayShift/array-shift.js
@@ -1,19 +1,19 @@
 'use strict';
 
 let insertShiftArray = (arr, val) => {
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] > val) {
-        let firstSlice = arr.slice(0, i);
-        firstSlice[firstSlice.length]= val;
-        let secondSlice = arr.slice(i, arr.length);
-        secondSlice.forEach(element => {
-          firstSlice[firstSlice.length]= element;
-        });
-        return firstSlice;
-      }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > val) {
+      let firstSlice = arr.slice(0, i);
+      firstSlice[firstSlice.length]= val;
+      let secondSlice = arr.slice(i, arr.length);
+      secondSlice.forEach(element => {
+        firstSlice[firstSlice.length]= element;
+      });
+      return firstSlice;
     }
-  };
+  }
+};
   
-  console.log(insertShiftArray([2,4,6,8], 5));
+console.log(insertShiftArray([2,4,6,8], 5));
   
  
