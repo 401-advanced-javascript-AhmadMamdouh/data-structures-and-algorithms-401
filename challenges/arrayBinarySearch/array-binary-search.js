7a51435909ffd071493b7ee785606ec9d15c4229
Ahmad Mamdouh ALhabrawi <mamdouh486@gmail.com>
2020-09-28 19:46:53 +0300

    solve eslint

--- challenges/arrayBinarySearch/array-binary-search.js
+++ challenges/arrayBinarySearch/array-binary-search.js
@@ -3,21 +3,21 @@
 
 var binarySearch = function(array, value) {
   var guess,
-      min = 0,
-      max = array.length - 1;	
+    min = 0,
+    max = array.length - 1;	
 
   while(min <= max){
-      guess = Math.floor((min + max) /2);
-if(array[guess] === value)
-    return guess;
-else if(array[guess] < value)
-    min = guess + 1;
-else
-    max = guess - 1;	
-   }
+    guess = Math.floor((min + max) /2);
+    if(array[guess] === value)
+      return guess;
+    else if(array[guess] < value)
+      min = guess + 1;
+    else
+      max = guess - 1;	
+  }
 
-   return -1;
-}
+  return -1;
+};
 
 
 console.log(binarySearch([4, 8, 15, 16, 23, 42], 15));
